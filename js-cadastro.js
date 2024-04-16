7bb4ad6e860c822522f3f2b92f8239064911b2c6
cpqd_audalio <audalio@cpqd.com.br>
2024-04-16 10:12:54 -0300

    7DaysCode - 2o dia

--- js-cadastro.js
+++ js-cadastro.js
@@ -24,12 +24,41 @@ function limpar() {
     iBirthDate.value = "";
 }
 
+function exibirDataFormatada(vData) {
+
+    var data = new Date(vData);
+    
+    var dia = String(data.getDate() + 1).padStart(2,'0');
+    var mes = String(data.getMonth() + 1).padStart(2,'0'); // Os meses começam do zero, então adicionamos 1
+    var ano = data.getFullYear();
+
+    var dataFormatada = dia + '/' + mes + '/' + ano;
+    return dataFormatada;
+}
+
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var dataNiver = exibirDataFormatada(iBirthDate.value);
+    console.log(iName.value + ": " + dataNiver);
+        
     //cadastrar();
-    console.log(iName.value + ": " + iBirthDate.value)
     //limpar();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var inputLetras = document.getElementById("name");
+
+    inputLetras.addEventListener("input", function() {
+        var valor = this.value.trim(); // Remove espaços em branco do início e do fim
+        var re = /^[A-Za-z]+$/; // Expressão regular para aceitar apenas letras
+        
+        if (!re.test(valor)) {
+            this.setCustomValidity("Por favor, insira apenas letras.");
+        } else {
+            this.setCustomValidity("");
+        }
+    });
 });
 
 // console.log(formulario)
